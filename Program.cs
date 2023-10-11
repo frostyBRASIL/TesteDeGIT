7012bcccddbef813ffc7449dc5ba96f6b149f8f2
Lucas Moura <lucasmoura278@gmail.com>
2023-10-11 16:25:46 -0300

    Modificando o arquivo teste.txt

--- Program.cs
+++ Program.cs
@@ -1,53 +1,15 @@
-﻿class Program
+﻿class ExercicioS
 {
+    static void ExerS()
+    {
+        for (int i = 15; i <= 200; i+= 3)
+        {
+            Console.WriteLine(i * i);
+        }
+    }
+
     static void Main()
     {
-        Console.WriteLine("Verificador de maiúsculas");
-        string text = Console.ReadLine();
-        int length = text.Length;
-        string alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        int cont = 0;
-
-        
-        for (int i = 0; i < length; i++) // Posição da letra do text
-        {
-            for (int j = 0; j < alfabeto.Length; j++) // Posição das letras do alfabeto
-            {
-                // Se text posição i bater com alguma letra maiúscula do alfabeto
-                // Incrementa o cont
-                if (text[i] == alfabeto[j])
-                {
-                    cont++;
-                    Console.WriteLine(alfabeto[j]);
-                }
-
-            }
-        }
-
-        for (int i = 0; i < text.Length; i++)
-        {
-            // Se (não) text na posição i for uma letra
-            // Decrementa o length
-            if (!char.IsLetter(text[i]))
-            {
-                length--;
-            }
-        }
-
-        Console.WriteLine(cont);
-        Console.WriteLine(length);
-
-
-        bool resul = false;
-        if (cont != length)
-        {
-            resul = resul;
-        }
-        else
-        {
-            resul = true;   
-        }
-
-        Console.WriteLine($"O resultado é: {resul}");
+        ExerS();
     }
 }
