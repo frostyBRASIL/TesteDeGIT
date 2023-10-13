ce454d3d0484ec8b8002a4b86ae8c2dcdebd53bb
Lucas Moura <lucasmoura278@gmail.com>
2023-10-13 15:35:15 -0300

    Adicionando modificações no código

--- Program.cs
+++ Program.cs
@@ -1,15 +1,42 @@
-﻿class ExercicioS
+﻿// https://leetcode.com/problems/palindrome-number/description/
+public class Solution 
 {
-    static void ExerS()
+    public static bool IsPalindrome(int x) 
     {
-        for (int i = 15; i <= 200; i+= 3)
-        {
-            Console.WriteLine(i * i);
-        }
-    }
+        string x_str = x.ToString();
+        int[] digs = new int [x_str.Length];
+        bool palindrome = false;
 
+        for (int i = 0; i < x_str.Length; i++)
+        {
+            digs[i] = int.Parse(digs[i].ToString());
+        }
+
+        int[] reversedDigs = new int[digs.Length];
+        Array.Copy(digs, reversedDigs, digs.Length);
+        Array.Reverse(reversedDigs);
+
+        for (int i = 0; i < digs.Length; i++)
+        {
+            if (digs[i] != reversedDigs[i])
+            {
+                palindrome = false;
+            }
+            else
+            {
+                palindrome = true;
+            }
+        }
+
+        return palindrome;
+
+    }
+}
+
+class Program
+{
     static void Main()
     {
-        ExerS();
+        Console.WriteLine(Solution.IsPalindrome(2021));
     }
 }
